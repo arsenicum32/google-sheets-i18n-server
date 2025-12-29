edf1810185c988eda0cb90b46a630905aac6346e
Arseniy <zombiehot@yandex.ru>
2025-12-29 15:40:18 +0300

    refactor: integrate cache into sheets client

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@ export default {
   port: process.env.PORT || 7996,
   googleApiKey: process.env.AUTH_KEY,
   tablesPath: process.env.TABLES_PATH || 'tables.txt',
+
+  cacheTtlMs: Number(process.env.CACHE_TTL_MS || 5000),
+  cacheStaleTtlMs: Number(process.env.CACHE_STALE_TTL_MS || 60_000),
 }
 
--- src/infrastructure/sheetsClient.js
+++ src/infrastructure/sheetsClient.js
@@ -1,23 +1,42 @@
 import { google } from 'googleapis'
 import config from '../config.js'
+import Cache from './cache.js'
+import logger from './logger.js'
 
 const sheets = google.sheets({
   version: 'v4',
   auth: config.googleApiKey,
 })
 
+const cache = new Cache({
+  ttlMs: config.cacheTtlMs,
+  staleTtlMs: config.cacheStaleTtlMs,
+  logger,
+})
+
+const fetchRows = (params) =>
+  new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.get(params, (error, response) => {
+      if (error) {
+        reject(error)
+        return
+      }
+
+      resolve(response?.data?.values || [])
+    })
+  })
+
 export default {
   getRows(params) {
-    return new Promise((resolve, reject) => {
-      sheets.spreadsheets.values.get(params, (error, response) => {
-        if (error) {
-          reject(error)
-          return
-        }
+    return cache.get(params, fetchRows)
+  },
 
-        resolve(response?.data?.values || [])
-      })
-    })
+  getCacheStats() {
+    return cache.getStats()
+  },
+
+  clearCache() {
+    cache.clear()
   },
 }
 
